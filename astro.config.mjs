aeb4dfdc4b448ec6412d904337064e0ceb4bfa0e
mvivopas <vivo@dribia.com>
2024-10-15 10:16:30 +0200

    add tailwind and areas de especialidad new section format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
@@ -8,5 +9,5 @@ import lit from "@astrojs/lit";
 export default defineConfig({
   site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  integrations: [sitemap(), mdx(), lit(), icon(), tailwind()], // Add renderers to the config
 });
